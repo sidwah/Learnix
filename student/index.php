062a731b1391df07ad9768292ca05485fede9eae
sidwah <sidwabarrock@gmail.com>
2025-03-26 06:24:21 +0000

    directory changed

--- student/index.php
+++ student/index.php
@@ -117,7 +117,7 @@
         <div class="mb-5">
           <div class="d-flex justify-content-between align-items-center mb-3">
             <h3 class="mb-0">In Progress</h3>
-            <a class="link" href="my-courses.php">View all <i class="bi-chevron-right small ms-1"></i></a>
+            <a class="link" href="enrolled-courses.php">View all <i class="bi-chevron-right small ms-1"></i></a>
           </div>
 
           <?php if (count($enrolled_courses) > 0): ?>
@@ -278,7 +278,7 @@
             <div class="text-center py-5">
               <img class="img-fluid mb-3" src="../assets/svg/illustrations/oc-medal.svg" alt="Empty state" width="200">
               <p>You haven't completed any courses yet.</p>
-              <a href="my-courses.php" class="btn btn-soft-primary">See Your Courses</a>
+              <a href="enrolled-courses.php" class="btn btn-soft-primary">See Your Courses</a>
             </div>
             <!-- End Empty State -->
           <?php endif; ?>
@@ -504,7 +504,7 @@
               <img class="img-fluid mb-3" src="../assets/svg/illustrations/oc-certificate.svg" alt="No certificates" width="200">
               <h5>No certificates yet</h5>
               <p>Complete a course to earn your first certificate.</p>
-              <a href="my-courses.php" class="btn btn-soft-primary">Continue Learning</a>
+              <a href="enrolled-courses.php" class="btn btn-soft-primary">Continue Learning</a>
             </div>
             <!-- End Empty State -->
           <?php endif; ?>
