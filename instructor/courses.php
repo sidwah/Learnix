b2de2a7064d64db0de9a7b421df82a8463403d26
sidwah <sidwabarrock@gmail.com>
2025-04-01 00:54:11 +0000

    fix: add missing newline at end of courses.php file

--- instructor/courses.php
+++ instructor/courses.php
@@ -499,4 +499,4 @@ ORDER BY c.updated_at DESC";
 
 <!-- Mirrored from coderthemes.com/hyper/saas/index.php by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 29 Jul 2022 10:20:07 GMT -->
 
-</html>c
+</html>
